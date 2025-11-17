a2cdbd0be57fbe54c96041931d6be0a767c56579
GhostDev <glowinghost@gmail.com>
2025-11-18 00:42:25 +0800

    Spaces to Tabs pt. 2

--- src/funkin/backend/scripting/events/game/NoteHitEvent.hx
+++ src/funkin/backend/scripting/events/game/NoteHitEvent.hx
@@ -5,26 +5,26 @@ import funkin.game.objects.Strum;
 import funkin.game.objects.Character;
 
 class NoteHitEvent extends CancellableEvent {
-    public var note:Note;
+	public var note:Note;
 
-    public var strumTime:Float;
-    public var noteData:Int;
-    public var character:Character;
-    public var characterID:Int;
-    public var noteType:String;
-    public var strum:Strum;
+	public var strumTime:Float;
+	public var noteData:Int;
+	public var character:Character;
+	public var characterID:Int;
+	public var noteType:String;
+	public var strum:Strum;
 	public var cpu:Bool;
 
-    public function new(note:Note) {
-        super();
+	public function new(note:Note) {
+		super();
 
-        this.note = note;
-        strumTime = note.strumTime;
-        noteData = note.noteData;
-        character = note.character;
-        characterID = note.characterID;
-        noteType = note.noteType;
-        strum = note.strum;
+		this.note = note;
+		strumTime = note.strumTime;
+		noteData = note.noteData;
+		character = note.character;
+		characterID = note.characterID;
+		noteType = note.noteType;
+		strum = note.strum;
 		cpu = strum.cpu;
-    }
+	}
 }
